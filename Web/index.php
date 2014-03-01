9237d50f9aff8bd482316f8806b61a58009f8928
pel5xq <pel5xq@virginia.edu>
2014-03-01 10:50:17 -0500

    Added Timespans To Weekly Patterns

--- Web/index.php
+++ Web/index.php
@@ -2,6 +2,7 @@
 
 require 'flight/Flight.php';
 
+//Library, Section, and Weekly Pattern (today)
 Flight::route('/library/@lib/section/@sec/day', function($lib, $sec) {
   	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
   	if (mysqli_connect_errno()) {
@@ -26,6 +27,7 @@ Flight::route('/library/@lib/section/@sec/day', function($lib, $sec) {
   	}
 });
 
+//Library, Section, and Weekly Pattern (specified)
 Flight::route('/library/@lib/section/@sec/day/@day', function($lib, $sec, $day) {
   	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
   	if (mysqli_connect_errno()) {
@@ -50,6 +52,7 @@ Flight::route('/library/@lib/section/@sec/day/@day', function($lib, $sec, $day) 
   	}
 });
 
+//Libraryand Weekly Pattern (today)
 Flight::route('/library/@lib/day', function($lib) {
   	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
   	if (mysqli_connect_errno()) {
@@ -74,6 +77,7 @@ Flight::route('/library/@lib/day', function($lib) {
   	}
 });
 
+//Library and Weekly Pattern (specified)
 Flight::route('/library/@lib/day/@day', function($lib, $day) {
   	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
   	if (mysqli_connect_errno()) {
@@ -96,6 +100,111 @@ Flight::route('/library/@lib/day/@day', function($lib, $day) {
       		http_response_code(500);
     	}
   	}
+});
+
+//(60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time)))
+
+//Library, Section, Timespan, and Weekly Pattern (today)
+Flight::route('/library/@lib/section/@sec/timespan/@minutes/day', function($lib, $sec, $minutes) {
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
+        and Library = ? and Section = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ? 
+        GROUP BY Library, Section")) {
+          $stmt->bind_param("ssd", $lib, $sec, $minutes);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
+});
+
+//Library, Section, Timespan, and Weekly Pattern (specified)
+Flight::route('/library/@lib/section/@sec/timespan/@minutes/day/@day', function($lib, $sec, $minutes, $day) {
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
+        and Library = ? and Section = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ?
+        GROUP BY Library, Section")) {
+          $stmt->bind_param("dssd", $day, $lib, $sec, $minutes);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
+});
+
+//Library Timespan, and Weekly Pattern (today)
+Flight::route('/library/@lib/timespan/@minutes/day', function($lib, $minutes) {
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
+        and Library = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ? GROUP BY Library")) {
+          $stmt->bind_param("sd", $lib, $minutes);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
+});
+
+//Library Timespan, and Weekly Pattern (specified)
+Flight::route('/library/@lib/timespan/@minutes/day/@day', function($lib, $minutes, $day) {
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
+        and Library = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ?
+        GROUP BY Library, Section")) {
+          $stmt->bind_param("dsd", $day, $lib, $minutes);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
 });
 
 Flight::start();
