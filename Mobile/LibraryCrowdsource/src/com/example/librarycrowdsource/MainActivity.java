3eccb384b00af894b3e66d34d0306435705db70a
pel5xq <pel5xq@virginia.edu>
2014-03-19 17:05:41 -0400

    Added Data Refreshing

--- Mobile/LibraryCrowdsource/src/com/example/librarycrowdsource/MainActivity.java
+++ Mobile/LibraryCrowdsource/src/com/example/librarycrowdsource/MainActivity.java
@@ -219,15 +219,36 @@ public class MainActivity extends Activity {
 		return true;
 	}
 	
-	/**protected void onResume(){
+	protected void onResume(){
 		super.onResume();
-		restartActivity();
+		reloadData();
 	}
 	
-	 private void restartActivity() { 
-         Intent i = new Intent(getBaseContext(), MainActivity.class); 
-         startActivity(i); 
-	 } */
+	 private void reloadData() { 
+         //Intent i = new Intent(getBaseContext(), MainActivity.class); 
+         //startActivity(i); 
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Alderman/timespan/60", 
+					""+R.id.aldermanCrowd, ""+R.id.aldermanNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Clark/timespan/60", 
+					""+R.id.clarkCrowd, ""+R.id.clarkNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Clemons/timespan/60", 
+					""+R.id.clemonsCrowd, ""+R.id.clemonsNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Commerce%20School/timespan/60", 
+					""+R.id.commerceCrowd, ""+R.id.commerceNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Rice/timespan/60", 
+					""+R.id.riceCrowd, ""+R.id.riceNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Thornton/timespan/60", 
+					""+R.id.thorntonCrowd, ""+R.id.thorntonNoise);
+			new MyAsyncTask().execute("http://plato.cs.virginia.edu/~pel5xq/"
+					+ "library/Wilsdorf/timespan/60", 
+					""+R.id.wilsdorfCrowd, ""+R.id.wilsdorfNoise);
+	 } 
 
 	private class MyAsyncTask extends AsyncTask<String, String, String> {
 
