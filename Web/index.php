75de65ab0143fefc6cd6cea612f07f1a531ef990
pel5xq <pel5xq@virginia.edu>
2014-03-23 14:17:08 -0400

    Updated Date Queries

--- Web/index.php
+++ Web/index.php
@@ -28,102 +28,102 @@ Flight::route('/insert/library/@lib/section/@sec/crowd/@crowd/noise/@noise', fun
 
 //Library, Section, and Weekly Pattern (today)
 Flight::route('/library/@lib/section/@sec/day', function($lib, $sec) {
-  	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
-  	if (mysqli_connect_errno()) {
-      	http_response_code(500);
-  	}
-  	else {
-    	$stmt = $db_connection->stmt_init();
-    	if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
-    		and Library = ? and Section = ? GROUP BY Library, Section")) {
-        	$stmt->bind_param("ss", $lib, $sec);
-        	$stmt->execute();
-        	$stmt->bind_result($Crowd, $Noise);
-        	$answer = array('crowd' => '0', 'noise' => '0');
-      		while($stmt->fetch()) {
-      			$answer = array('crowd' => $Crowd, 'noise' => $Noise);
-      		}
-      		echo json_encode($answer);
-    	}
-    	else {
-      		http_response_code(500);
-    	}
-  	}
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
+        and Library = ? and Section = ? GROUP BY Library, Section")) {
+          $stmt->bind_param("ss", $lib, $sec);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
 });
 
 //Library, Section, and Weekly Pattern (specified)
 Flight::route('/library/@lib/section/@sec/day/@day', function($lib, $sec, $day) {
-  	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
-  	if (mysqli_connect_errno()) {
-      	http_response_code(500);
-  	}
-  	else {
-    	$stmt = $db_connection->stmt_init();
-    	if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
-    		and Library = ? and Section = ? GROUP BY Library, Section")) {
-        	$stmt->bind_param("dss", $day, $lib, $sec);
-        	$stmt->execute();
-        	$stmt->bind_result($Crowd, $Noise);
-        	$answer = array('crowd' => '0', 'noise' => '0');
-      		while($stmt->fetch()) {
-      			$answer = array('crowd' => $Crowd, 'noise' => $Noise);
-      		}
-      		echo json_encode($answer);
-    	}
-    	else {
-      		http_response_code(500);
-    	}
-  	}
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
+        and Library = ? and Section = ? GROUP BY Library, Section")) {
+          $stmt->bind_param("dss", $day, $lib, $sec);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
 });
 
 //Libraryand Weekly Pattern (today)
 Flight::route('/library/@lib/day', function($lib) {
-  	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
-  	if (mysqli_connect_errno()) {
-      	http_response_code(500);
-  	}
-  	else {
-    	$stmt = $db_connection->stmt_init();
-    	if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
-    		and Library = ? GROUP BY Library")) {
-        	$stmt->bind_param("s", $lib);
-        	$stmt->execute();
-        	$stmt->bind_result($Crowd, $Noise);
-        	$answer = array('crowd' => '0', 'noise' => '0');
-      		while($stmt->fetch()) {
-      			$answer = array('crowd' => $Crowd, 'noise' => $Noise);
-      		}
-      		echo json_encode($answer);
-    	}
-    	else {
-      		http_response_code(500);
-    	}
-  	}
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = DAYOFWEEK(NOW()) 
+        and Library = ? GROUP BY Library")) {
+          $stmt->bind_param("s", $lib);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
 });
 
 //Library and Weekly Pattern (specified)
 Flight::route('/library/@lib/day/@day', function($lib, $day) {
-  	$db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
-  	if (mysqli_connect_errno()) {
-      	http_response_code(500);
-  	}
-  	else {
-    	$stmt = $db_connection->stmt_init();
-    	if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
-    		and Library = ? GROUP BY Library, Section")) {
-        	$stmt->bind_param("ds", $day, $lib);
-        	$stmt->execute();
-        	$stmt->bind_result($Crowd, $Noise);
-        	$answer = array('crowd' => '0', 'noise' => '0');
-      		while($stmt->fetch()) {
-      			$answer = array('crowd' => $Crowd, 'noise' => $Noise);
-      		}
-      		echo json_encode($answer);
-    	}
-    	else {
-      		http_response_code(500);
-    	}
-  	}
+    $db_connection = new mysqli('stardock.cs.virginia.edu', 'cs4720pel5xq', '2014spring', 'cs4720pel5xq');
+    if (mysqli_connect_errno()) {
+        http_response_code(500);
+    }
+    else {
+      $stmt = $db_connection->stmt_init();
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DAYOFWEEK(TIMESTAMP(Time)) = ? 
+        and Library = ? GROUP BY Library, Section")) {
+          $stmt->bind_param("ds", $day, $lib);
+          $stmt->execute();
+          $stmt->bind_result($Crowd, $Noise);
+          $answer = array('crowd' => '0', 'noise' => '0');
+          while($stmt->fetch()) {
+            $answer = array('crowd' => $Crowd, 'noise' => $Noise);
+          }
+          echo json_encode($answer);
+      }
+      else {
+          http_response_code(500);
+      }
+    }
 });
 
 //Library, Section, Timespan, and Weekly Pattern (today)
@@ -237,7 +237,7 @@ Flight::route('/library/@lib/section/@sec/timespan/@minutes', function($lib, $se
     }
     else {
       $stmt = $db_connection->stmt_init();
-      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE  
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DATE(Time) = CURDATE() and
         Library = ? and Section = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ?
         GROUP BY Library, Section")) {
           $stmt->bind_param("ssd", $lib, $sec, $minutes);
@@ -263,7 +263,7 @@ Flight::route('/library/@lib/timespan/@minutes', function($lib, $minutes) {
     }
     else {
       $stmt = $db_connection->stmt_init();
-      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE
+      if($stmt->prepare("SELECT AVG(Crowd), AVG(Noise) FROM `LibraryCrowd` WHERE DATE(Time) = CURDATE() and
         Library = ? and (60*ABS(HOUR(Now()) - HOUR(Time)) + ABS(MINUTE(Now()) - MINUTE(Time))) <= ?
         GROUP BY Library, Section")) {
           $stmt->bind_param("sd", $lib, $minutes);
