17bd957d2f458d78a12e48406071bf7164ea0e4e
pel5xq <pel5xq@virginia.edu>
2014-03-23 15:37:29 -0400

    Added Insert Command to Node

--- librarynode/server.js
+++ librarynode/server.js
@@ -12,24 +12,26 @@ http.createServer(function (request, response) {
 	var pathname = url.parse(request.url).pathname;
 	var url_parts = url.parse(request.url, true);
 	var query = url_parts.query;
+	var url_split = pathname.split("/");
 
-	if (pathname == "/") {
-		console.log("QueryAll");
-		connection.query('SELECT * FROM LibraryStudyGroups;', 
+	if (url_split[1] == "insert" &&
+		url_split[2] == "library" &&
+		url_split[4] == "section" &&
+		url_split[6] == "department" &&
+		url_split[8] == "coursenum" &&
+		url_split[10] == "name" &&
+		url_split[12] == "description" &&
+		url_split[14] == "start" &&
+		url_split[16] == "end") {
+		console.log(url_split[3]+", "+url_split[5]+", "+url_split[7]+", "+url_split[9]+
+			", "+url_split[11]+", "+url_split[13]+", "+url_split[15]+", "+url_split[17]);
+		connection.query('INSERT INTO `LibraryStudyGroups`(`Library`, `Section`, `Dept`, `CourseNum`, `Name`, `Descrip`, `StartTime`, `EndTime`) VALUES ('
+				+connection.escape(url_split[3])+','+connection.escape(url_split[5])+','+connection.escape(url_split[7])+','+connection.escape(url_split[9])+','
+				+connection.escape(decodeURI(url_split[11]))+','+connection.escape(decodeURI(url_split[13]))+',TimeStamp('+connection.escape(url_split[15])
+				+'),TimeStamp('+connection.escape(url_split[17])+'))', 
 			function(error, rows, fields) {
 			response.writeHead(200, {'Content-Type': 'text/plain'});
-			response.write(JSON.stringify(rows));
 			response.end();
 		});
 	}
-	else if (pathname != "/favicon.ico"){
-		console.log("QueryLibrary "+pathname.substring(1));
-		connection.query('SELECT * FROM LibraryStudyGroups WHERE Library = '+
-			connection.escape(pathname.substring(1))+';', 
-			function(error, rows, fields) {
-			response.writeHead(200, {'Content-Type': 'text/plain'});
-			response.write(JSON.stringify(rows));
-			response.end();
-		});
-	}
-}).listen(process.env.PORT || 8080);
+}).listen(process.env.PORT || 8080)
